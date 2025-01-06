5bff73313974ce22d31bc57edd676705101506f2
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2025-01-06 18:54:15 +0100

    Update GeoTiff.js

--- examples/GeoTiff.js
+++ examples/GeoTiff.js
@@ -65,7 +65,9 @@ requirejs(['./WorldWindShim',
         geoTiffLayer.showSpinner = true;
         wwd.addLayer(geoTiffLayer);
 
-        var resourceUrl = "https://worldwind.arc.nasa.gov/web/examples/data/black_sea_rgb.tif";
+var resourceUrl = "https://aibolem.github.io/chart-nuclide/example/black_sea_rgb.tif";
+               
+      //  var resourceUrl = "https://worldwind.arc.nasa.gov/web/examples/data/black_sea_rgb.tif";
 
         // Load the GeoTiff using the Reader's built-in XHR retrieval function.
         WorldWind.GeoTiffReader.retrieveFromUrl(resourceUrl, function (geoTiffReader, xhrStatus) {
